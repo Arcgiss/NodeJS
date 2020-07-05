b32b6b37cfcd5f5d1a109d64e0e280252e37ef92
Arcgiss <58184891+Arcgiss@users.noreply.github.com>
2020-07-06 00:55:00 +0300

    mangoose

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -3,12 +3,20 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    admin:   {
+        type: Boolean,
+        default: false
     }
 });
+
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
